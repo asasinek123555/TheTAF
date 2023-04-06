3236f373ce4f9f053066d830a7efb89e8a3cf2e1
sm49389 <sm49389@zut.edu.pl>
2023-04-06 14:45:52 +0200

    naprawa znaczy nie naprawa poprawa (refactor)

--- TestAutomationFramework/src/func/element/scrollToElement.js
+++ TestAutomationFramework/src/func/element/scrollToElement.js
@@ -1,8 +1,6 @@
 import { driver } from "../../util.js";
-import { By } from "selenium-webdriver";
 export const scrollToElement = async (elem) => {
-    const elemToScroll = await driver.findElement(By.css(elem))
     await driver.actions()
-        .scroll(0, 0, 0, 0, elemToScroll)
+        .scroll(0, 0, 0, 0, elem)
         .perform()
   };
