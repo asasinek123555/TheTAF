7b264f507689d3f5d8f6205872bb9bcc0bc8ec55
Xewol <bartekdeluga0702@gmail.com>
2023-04-20 13:38:03 +0200

    fixes

--- TestAutomationFramework/src/index.js
+++ TestAutomationFramework/src/index.js
@@ -1,23 +1,29 @@
-
 // imports
 import { finalize } from "./util.js";
 import { getUrl, goToUrl, scrollToElement } from "./func/index.js";
-
+import { generate } from "./util.js";
 export const taf = {
-  goToUrl, getUrl,
+  goToUrl,
+  getUrl,
   scrollToElement,
 };
 
 export async function test(name, callback) {
-  //TODO save test name
+  let start, end, state;
 
-  //call callback and measure time
-  const start = performance.now();
-  await callback();
-  const end = performance.now();
-
-  generate(name, end - start);
-  //clear self
-
-  finalize();
+  try {
+    start = performance.now();
+    //call callback and measure time
+    await callback();
+    end = performance.now();
+  } catch (error) {
+    state = 0;
+    end = performance.now();
+  } finally {
+    state = 1;
+    console.log(end, start);
+    generate(name, end - start, state);
+    //clear self
+    finalize();
+  }
 }
