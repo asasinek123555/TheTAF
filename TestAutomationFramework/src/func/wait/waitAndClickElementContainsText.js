3cc1cd71084e1a75afd99cac5f4f683193f9a503
asasinek123555 <xyz1a2b@poczta.onet.pl>
2023-05-11 11:26:25 +0200

    repair missing argument

--- TestAutomationFramework/src/func/wait/waitAndClickElementContainsText.js
+++ TestAutomationFramework/src/func/wait/waitAndClickElementContainsText.js
@@ -1,6 +1,6 @@
 import { driver } from "../../util.js";
 
-export const waitAndClickElementContainsText = async (url) => {
+export const waitAndClickElementContainsText = async (url,maxTime=60000) => {
     let element = driver.By.xpath("//*[contains(text(), '"+text+"')]")
     if(!await driver.wait(driver.until.elementIsVisible(
         element,
